77183b7c78e521f0c1a3a0e679393301a5294ed0
vanessa-campos <70600785+vanessa-campos@users.noreply.github.com>
2022-09-10 10:52:52 -0300

    erro5_corrigido

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -52,6 +52,7 @@ export default function App() {
         calculator()
         return
       case '+/-':
+        setCurrentNumber((currentNumber * -1).toString())
         return
       case '%':
         setLastNumber(currentNumber + " % ")
